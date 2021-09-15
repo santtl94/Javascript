1de2fc4deb6cdae57ef64d92c43d963b197f65d5
unknown <you@example.com>
2021-09-15 16:28:27 -0300

    repo

---
+++ Js/app.js
@@ -0,0 +1,4 @@
+let suEdad = parseInt(prompt('Ingrese su edad'));
+let edadMascota = parseInt(prompt("Ingrese la edad de su mascota"));
+let sumaEdades = suEdad + edadMascota;
+let edadTotal = alert('Su edad total es de'+ ' ' + sumaEdades);
